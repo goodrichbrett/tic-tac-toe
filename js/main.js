e4d5e8d506eb90a5c13b898f429a5116da81e9c9
goodrichbrett <brettgoodrich@outlook.com>
2020-08-02 18:39:29 -0500

    add win and tie logic, status changes to show current player's turn, and shows winning and tie message

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /*------Constants------*/
 
 const colors = {
-	"1": "#f2d53c",
-	"-1": "#c80e13",
+	"1": "#ccff2d",
+	"-1": "#ff2d2d",
 	null: "#7d3cff",
 };
 
@@ -48,6 +48,9 @@ function init() {
 	turn = 1;
 	winner = null;
 	render();
+	// set the status to waiting for player
+	status.innerText = "X = King ♔ // O = Queen ♕";
+	status.style.color = "#545454";
 }
 
 // Function to handle click events on the squares
@@ -60,26 +63,78 @@ function handleClick(e) {
 	if (e.target.classList[2] === "x" || e.target.classList[2] === "o") {
 		return;
 	}
-	// add class list 'x' or 'o' depending on turn
-	if (turn === 1) {
-		e.target.style.color = colors[turn];
-		e.target.classList.add("x");
-	} else {
-		e.target.style.color = colors[turn];
-		e.target.classList.add("o");
+	// Call addClassList to switch between x/o and designated colors
+	addClassList(e.target);
+	// Call updateBoardArray to update board and change turns
+	updateBoardArray(clickedSquareIdx);
+}
+
+// Compares current board to winningCombos to decide if there is a winner
+function checkForWinner() {
+	const squaresArray = Array.from(squares);
+	let xSquares = [];
+	let oSquares = [];
+	for (const square of squaresArray) {
+		if (square.classList[2] === "x") {
+			xSquares.push(parseInt(square.classList[1]));
+		} else if (square.classList[2] === "o") {
+			oSquares.push(parseInt(square.classList[1]));
+		}
 	}
-	//update board array, change turns
+	let checkedX = checkForWinningCombo(xSquares);
+	let checkedO = checkForWinningCombo(oSquares);
+	if (checkedX === 1) return 1;
+	if (checkedO * -1 === -1) return -1;
+	if (board.includes(null)) {
+		return null;
+	} else {
+		return "T";
+	}
+}
+
+function addClassList(e) {
+	if (turn === 1) {
+		e.style.color = colors[turn];
+		e.classList.add("x");
+	} else {
+		e.style.color = colors[turn];
+		e.classList.add("o");
+	}
+}
+
+function updateBoardArray(clickedSquareIdx) {
 	board[clickedSquareIdx] = turn;
 	turn *= -1;
 	winner = checkForWinner();
 	render();
 }
 
-// Compares current board to winningCombos to decide if there is a winner
-function checkForWinner() {
-	//
+function checkForWinningCombo(arr) {
+	for (let i = 0; i < winningCombos.length; i++) {
+		if (JSON.stringify(arr).includes(JSON.stringify(winningCombos[i]))) {
+			return 1;
+		}
+	}
 }
 
-// Render function:
-// Displays the current state of the board on the page, updating the elements to reflect either X or O depending on whose turn it is
-function render() {}
+// Change the status to display turn, add corresponding color to status
+// Change status to display the winner with the correct color text
+function render() {
+	if (turn === 1) {
+		status.innerHTML = "Make your move, King ♔";
+		status.style.color = "#ccff2d";
+	} else if (turn === -1) {
+		status.innerText = "Make your move, Queen ♛";
+		status.style.color = "#ff2d2d";
+	}
+	if (winner === 1) {
+		status.style.color = "#ccff2d";
+		status.innerText = "The King wins!";
+	} else if (winner === -1) {
+		status.style.color = "#ff2d2d";
+		status.innerText = "The Queen wins!";
+	} else if (winner === "T") {
+		status.style.color = "#545454";
+		status.innerText = "Tie game!";
+	}
+}
